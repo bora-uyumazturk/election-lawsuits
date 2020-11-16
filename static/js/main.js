d7563cbda1c15d61ee5ccd33db4239437e679634
bora-uyumazturk <bora.uyumazturk@gmail.com>
2020-11-15 20:19:47 -0500

    Basic version of animation

---
+++ static/js/main.js
@@ -0,0 +1,53 @@
+const data_address = 'https://raw.githubusercontent.com/bora-uyumazturk/election-lawsuits/main/data/sample_data.csv';
+
+const width = 500;
+const height = 300;
+
+function getCaseStates(data, date) {
+  // get last sate of all cases before given date
+  return _.chain(data)
+   .filter((x) => {return x.Date <= date;})
+   .groupBy('case_id')
+   .map((x) => {return x[x.length - 1];})
+   .flatten()
+   .value()
+}
+
+function main() {
+  // begin downloading data
+  let promises = [
+    d3.csv(data_address)
+  ]
+
+  Promise.all(promises).then(function(allData) {
+
+    let lawsuitData = allData[0];
+
+    let dateLabel = new DateLabel('#date-label');
+    let graph = new StateGrid('#chart-area', width, height);
+
+    var i = 4;
+    var dateStr;
+    var curDate;
+
+    const interval = setInterval(() => {
+      // get date string
+      dateStr = `${i}`;
+      if (i < 10) {
+        dateStr = `0${i}`;
+      }
+      curDate = `2020-11-${dateStr}`;
+
+      // update visuals
+      dateLabel.update(curDate);
+      graph.update(getCaseStates(lawsuitData, curDate));
+
+      i++;
+      if (i > 14) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  })
+}
+
+main();
---
+++ static/js/stategrid.js
@@ -0,0 +1,109 @@
+StateGrid = function(_parentElement, width, height) {
+  this.parentElement = _parentElement;
+  this.width = width;
+  this.height = height;
+  this.margin_top = 50;
+  this.r = 10;
+  this.pad = 2;
+  this.initVis();
+}
+
+StateGrid.prototype.initVis = function () {
+  this.yScale = d3.scaleBand()
+    .domain(["Denied", "Appeal", "Complaint"])
+    .range([this.height + this.margin_top, this.margin_top]);
+
+  this.fillScale = d3.scaleOrdinal()
+    .domain(["Denied", "Appeal", "Complaint"])
+    .range(["#FF006B", "#eef086", "#33CCCC"]);
+
+  var vis = this;
+
+  vis.svg = d3.select(vis.parentElement)
+    .append("svg")
+    .attr("width", this.width)
+    .attr("height", this.height);
+
+  vis.svg.g = vis.svg.append("g");
+
+  this.initLabels();
+}
+
+StateGrid.prototype.initLabels = function () {
+  // init labels
+  this.svg.g.selectAll("text")
+    .data(["Denied", "Appeal", "Complaint"])
+    .enter()
+    .append("text")
+    .attr("x", 0)
+    .attr("y", (d) => {
+      return this.yScale(d) + 5;
+    })
+    .text((d) => {
+      return d;
+    });
+}
+
+StateGrid.prototype.update = function (data) {
+  console.log(data);
+  var g = this.svg.g;
+
+  // get index within group
+  var groupId = {};
+  var groupCounter = {
+    'Denied': 0,
+    'Appeal': 0,
+    'Complaint': 0
+  }
+
+  for (var i = 0; i < data.length; i++) {
+    var group = data[i].action;
+    groupId[data[i].case_id] = groupCounter[data[i].action]
+    groupCounter[data[i].action] += 1;
+  }
+
+
+  // enter => update => exit
+  var u = g.selectAll('circle')
+    .data(data, (d) => {
+      return d.case_id;
+    })
+
+  u.enter()
+    .append('circle')
+    .attr('cx', 150)
+    .attr('cy', (d) => {
+      return this.yScale(d.action);
+    })
+    .attr('r', this.r)
+    .attr('fill', (d) => {
+      return this.fillScale(d.action);
+    })
+    .style('fill-opacity', 0.0)
+    .attr('stroke', (d) => {
+      return this.fillScale(d.action);
+    })
+    .attr('stroke-opacity', 0.0)
+    .merge(u)
+    .transition()
+    // TODO: modify both x and y positions for when
+    // there are more elements
+    .attr('cx', (d) => {
+      return 150 + groupId[d.case_id]*2*(this.r + this.pad);
+    })
+    .attr('cy', (d) => {
+      return this.yScale(d.action);
+    })
+    .attr('fill', (d) => {
+      return this.fillScale(d.action);
+    })
+    .attr('stroke', (d) => {
+      return this.fillScale(d.action);
+    })
+    .style('fill-opacity', 0.3)
+    .attr('stroke-opacity', 1.0);
+
+    u.exit()
+      .remove();
+
+}
